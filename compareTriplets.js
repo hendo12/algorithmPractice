6ae4dcafd934376647eec94f993c652137adc076
Henry Doce <henrydoce@protonmail.com>
2020-10-11 15:58:08 -0400

    Another HackerRank

---
+++ compareTriplets.js
@@ -0,0 +1,14 @@
+function compareTriplets(a, b) {
+  let alicePoints = 0;
+  let bobPoints = 0;
+
+  for ( let i = 0; i < 3; i ++) {
+    if(a[i] > b[i]) {
+      alicePoints++;
+    } else if(a[i] < b[i]) {
+      bobPoints ++
+    }
+  }
+
+  return [alicePoints, bobPoints]
+}
